7464743cf358f3788fc72087091d3c2bd91038b1
JuKu <kuenzel.justin@t-online.de>
2021-11-06 00:23:04 +0100

    Change AttributeType

--- commons/src/main/java/com/jukusoft/opendmx/commons/fixture/AttributeType.java
+++ commons/src/main/java/com/jukusoft/opendmx/commons/fixture/AttributeType.java
@@ -9,11 +9,29 @@ public enum AttributeType {
 
 	POSITION("pos"),
 
+	DIMMER("dim"),
+
+	SHUTTER("shutter"),
+
+	FOCUS("focus"),
+
+	ZOOM("zoom"),
+
 	MOVEMENT_SPEED("movSpeed"),
 
 	COLOR_RAD("colR"),
 
-	COLOR("col");
+	COLOR("col"),
+
+	GOBO("gobo"),
+
+	GOBO_ROT("goboRot"),
+
+	PRISM("prism"),
+
+	FUNCTION("func"),
+
+	CONTROL("control");
 
 	private final String shortName;
 
