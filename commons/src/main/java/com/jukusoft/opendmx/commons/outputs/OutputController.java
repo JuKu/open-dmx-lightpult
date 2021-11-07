208db9dd43a6ae682daeeeed41c9cfe13cfd9459
Justin Kuenzel <kuenzel.justin@t-online.de>
2021-11-08 00:08:50 +0100

    Change OutputController

--- commons/src/main/java/com/jukusoft/opendmx/commons/outputs/OutputController.java
+++ commons/src/main/java/com/jukusoft/opendmx/commons/outputs/OutputController.java
@@ -16,10 +16,24 @@ public interface OutputController {
     public String getName();
 
     /**
+     * a generated string which describes the device, e.q. the Manufacturer, Model and the IP address and so an.
+     *
+     * @return a description string for the device
+     */
+    public String getDeviceDetails();
+
+    /**
      * get the count of the supported DMX universes by this device.
      *
      * @return number of supported DMX universes by this device
      */
     public int getSupportedDMXUniverses();
+
+    /**
+     * check, if the device is connected.
+     *
+     * @return true, if the device is connected
+     */
+    public boolean isConnected();
 
 }
