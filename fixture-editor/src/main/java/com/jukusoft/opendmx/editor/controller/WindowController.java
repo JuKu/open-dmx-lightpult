20d09f2032634bfda29e40a1c0cd4bb9cbca2ea1
JuKu <kuenzel.justin@t-online.de>
2021-11-13 18:51:09 +0100

    Add DialogUtils

--- fixture-editor/src/main/java/com/jukusoft/opendmx/editor/controller/WindowController.java
+++ fixture-editor/src/main/java/com/jukusoft/opendmx/editor/controller/WindowController.java
@@ -1,15 +1,23 @@
 package com.jukusoft.opendmx.editor.controller;
 
+import com.jukusoft.opendmx.commons.fixture.FixtureLibEntry;
+import com.jukusoft.opendmx.commons.fixture.FixtureLibrary;
+import com.jukusoft.opendmx.commons.utils.Dirs;
+import com.jukusoft.opendmx.editor.utils.DialogUtils;
 import com.jukusoft.opendmx.editor.utils.Version;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TabPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.text.DecimalFormat;
 import java.util.Objects;
@@ -20,16 +28,33 @@ public class WindowController implements Initializable {
     @FXML
     protected Label versionLabel;
 
+	@FXML
+	protected TabPane tabPane;
+
+	/**
+	 * the file path to the open .fixture file (if null, no fixture is opened).
+	 */
+	private String currentFilePath;
+
+	/**
+	 * the main stage.
+	 */
+	private Stage mainStage;
+
     /**
      * initialize window, will be called after loading fxml file
      */
     public void init(Stage stage, Scene scene, Pane pane) {
         //Objects.requireNonNull(calcButton);
         Objects.requireNonNull(versionLabel);
+        Objects.requireNonNull(tabPane);
+
+		this.mainStage = stage;
 
 		refreshVersion();
 
-		//TODO: add code here
+		//remove all tabs
+		this.tabPane.getTabs().clear();
     }
 
     @FXML
@@ -47,7 +72,24 @@ public class WindowController implements Initializable {
 	 * create a new fixture - this method is called from the menu.
 	 */
 	public void newFixture() {
-		// TODO: add code here
+		// the user should choose a filename first
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.setTitle("Choose path for new fixture file");
+		fileChooser.setInitialDirectory(new File(Dirs.DATA + "fixtures/"));
+
+		//Set extension filter for text files
+		FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Fixture files (*.fixture)", "*.fixture");
+		fileChooser.getExtensionFilters().add(extFilter);
+
+		//Show save file dialog
+		File file = fileChooser.showSaveDialog(this.mainStage);
+
+		if (file != null) {
+			FixtureLibEntry fixture = new FixtureLibEntry("", "", "");
+
+			//TODO: store path and create new file
+			saveFixture(file, fixture);
+		}
 	}
 
 	/**
@@ -61,7 +103,16 @@ public class WindowController implements Initializable {
 	 * save a fixture file - this method is called from the menu.
 	 */
 	public void saveFixtureDialog() {
-		//
+		//Show save file dialog
+	}
+
+	protected void saveFixture(File file, FixtureLibEntry fixture) {
+		FixtureLibrary library = new FixtureLibrary();
+		try {
+			library.save(file, fixture);
+		} catch (IOException e) {
+			DialogUtils.showExceptionDialog("Error!", "An exception occured while trying to save the fixture.", "", e);
+		}
 	}
 
 	/**
---
+++ fixture-editor/src/main/java/com/jukusoft/opendmx/editor/utils/DialogUtils.java
@@ -0,0 +1,68 @@
+package com.jukusoft.opendmx.editor.utils;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+/*
+ * This utility class creates and opens small dialogs (e.q. for warnings).
+ *
+ * @author Justin Kuenzel
+ */
+public class DialogUtils {
+
+	/**
+	 * private constructor, so noone can create an instance of this class.
+	 */
+	private DialogUtils() {
+		//
+	}
+
+	/**
+	 * this methods creates and opens an exception dialog.
+	 *
+	 * @param title title of the dialog
+	 * @param headerText header text of the dialog
+	 * @param content content of the dialog
+	 * @param ex the exeption to show
+	 */
+	public static void showExceptionDialog(String title, String headerText, String content, Exception ex) {
+		Alert alert = new Alert(Alert.AlertType.ERROR);
+		alert.setTitle(title);
+		alert.setHeaderText(headerText);
+		alert.setContentText(content);
+
+		// Create expandable Exception, see also: https://code.makery.ch/blog/javafx-dialogs-official/
+		StringWriter sw = new StringWriter();
+		PrintWriter pw = new PrintWriter(sw);
+		ex.printStackTrace(pw);
+		String exceptionText = sw.toString();
+
+		Label label = new Label("The exception stacktrace was:");
+
+		TextArea textArea = new TextArea(exceptionText);
+		textArea.setEditable(false);
+		textArea.setWrapText(true);
+
+		textArea.setMaxWidth(Double.MAX_VALUE);
+		textArea.setMaxHeight(Double.MAX_VALUE);
+		GridPane.setVgrow(textArea, Priority.ALWAYS);
+		GridPane.setHgrow(textArea, Priority.ALWAYS);
+
+		GridPane expContent = new GridPane();
+		expContent.setMaxWidth(Double.MAX_VALUE);
+		expContent.add(label, 0, 0);
+		expContent.add(textArea, 0, 1);
+
+		// show extenable content (exception) in the dialog.
+		alert.getDialogPane().setExpandableContent(expContent);
+
+		alert.showAndWait();
+	}
+
+}
