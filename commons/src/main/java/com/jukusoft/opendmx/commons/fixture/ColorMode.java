8d7ee7a7bd782dd7aff166aa27d836a3014150f3
JuKu <kuenzel.justin@t-online.de>
2021-11-14 00:25:39 +0100

    Add ColorMode

---
+++ commons/src/main/java/com/jukusoft/opendmx/commons/fixture/ColorMode.java
@@ -0,0 +1,15 @@
+package com.jukusoft.opendmx.commons.fixture;
+
+/**
+ * All available color modes for fixtures.
+ *
+ * @author Justin Kuenzel
+ */
+public enum ColorMode {
+
+	//RGB, RGBW, RGBWA and so on.
+	RGB,
+
+	CMY
+
+}
